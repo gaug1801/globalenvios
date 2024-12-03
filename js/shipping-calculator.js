ffabef267ace88baf8073eb0f249728a96b77cf2
gaug1801 <gaug1801@gmail.com>
2024-12-03 12:04:15 -0600

    Added 'back' button functionality and added footer to webpage

--- js/shipping-calculator.js
+++ js/shipping-calculator.js
@@ -1,31 +1,31 @@
 import {formatCurrency} from "./utils/money.js";
 const volumeConstantCents = 178200;
 const vzlaPricePerFootCents = 3300;
-
 const previousButtonElement = document.querySelector('.js-prev-button');
 const nextButtonElement = document.querySelector('.js-next-button');
-const inputZipElement = document.querySelector('.js-zip-input');
-const inputHeightElement = document.querySelector('.js-height-input');
-const inputLengthElement = document.querySelector('.js-length-input');
-const inputWidthElement = document.querySelector('.js-width-input');
-const inputWeightElement = document.querySelector('.js-weight-input');
 
 nextButtonElement.addEventListener('click', ()=> {
-  let zip = inputZipElement.value;
-  let height = inputHeightElement.value;
-  let length = inputLengthElement.value;
-  let width = inputWidthElement.value;
-  let weight = inputWeightElement.value;
+  document.querySelector('#step-one').classList.add('last-step');
+  document.querySelector('#step-one').classList.remove('current-step');
+  document.querySelector('#step-two').classList.add('current-step');
+  document.querySelector('#step-two').classList.remove('next-step');
+
+  let zip = document.querySelector('.js-zip-input').value;
+  let height = document.querySelector('.js-height-input').value;
+  let length = document.querySelector('.js-length-input').value;
+  let width = document.querySelector('.js-width-input').value;
+  let weight = document.querySelector('.js-weight-input').value;
+  let city = document.querySelector('.js-city-input').value;
   let boxVolume = (length * width * height);
 
   let boxPrice = formatCurrency(((boxVolume * 100) / volumeConstantCents) * vzlaPricePerFootCents);
-
-  console.log(boxPrice);
 
   localStorage.setItem('zip', JSON.stringify(zip));
   localStorage.setItem('height', JSON.stringify(height));
   localStorage.setItem('length', JSON.stringify(length));
   localStorage.setItem('width', JSON.stringify(width));
+  localStorage.setItem('weight', JSON.stringify(weight));
+  localStorage.setItem('city', JSON.stringify(city));
 
   let html = 
   `
@@ -42,13 +42,29 @@ nextButtonElement.addEventListener('click', ()=> {
   
   localStorage.setItem('previous-html', JSON.stringify(previousHTML));
 
-  // console.log(JSON.parse(localStorage.getItem('previous-html')));
   document.querySelector('.calculator-step-one').innerHTML = html;
   document.querySelector('.js-prev-button').classList.add('is-visible');
 });
 
 previousButtonElement.addEventListener('click', ()=> {
-  document.querySelector('.calculator-step-one').innerHTML = JSON.parse(localStorage.getItem('previous-html'));
-  previousButtonElement.classList.remove('is-visible');
+  previousButtonElement.classList.remove('is-visible')
 
+  document.querySelector('#step-one').classList.add('current-step');
+  document.querySelector('#step-one').classList.remove('last-step');
+  document.querySelector('#step-two').classList.add('next-step');
+  document.querySelector('#step-two').classList.remove('current-step');
+
+  document.querySelector('.calculator-step-one').innerHTML 
+    = convertObjectToString(('previous-html'));
+  document.querySelector('.js-city-input').value= convertObjectToString('city');
+  document.querySelector('.js-zip-input').value = convertObjectToString('zip');
+  document.querySelector('.js-height-input').value = convertObjectToString('height');
+  document.querySelector('.js-length-input').value = convertObjectToString('length');
+  document.querySelector('.js-width-input').value = convertObjectToString('width');
+  document.querySelector('.js-weight-input').value = convertObjectToString('weight');
+  document.querySelector('.js-city-input').value = convertObjectToString('city');
 });
+
+function convertObjectToString(string){
+  return JSON.parse(localStorage.getItem(string))
+}
