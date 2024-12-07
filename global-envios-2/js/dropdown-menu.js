71139b248e16fd6c566775757af0160ac9d30701
gaug1801 <gaug1801@gmail.com>
2024-12-06 18:53:51 -0600

    Delete old files

--- global-envios-2/js/dropdown-menu.js
+++
@@ -1,8 +0,0 @@
-let btn = document.querySelector('#btn')
-let sidebar = document.querySelector('.dropdown-menu')
-let modal = document.querySelector('#myModal');
-
-btn.onclick = function() {
-  sidebar.classList.toggle('active');
-  modal.classList.toggle('active');
-}
